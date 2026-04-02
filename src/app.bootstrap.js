0869be29ae6ec461630459531e0e647f518f0bac
Rahma108 <salamarahma667@gmail.com>
2026-04-02 05:57:45 +0200

    Return

--- src/app.bootstrap.js
+++ src/app.bootstrap.js
@@ -1,11 +1,11 @@
 
 import express from 'express'
 import { authRouter , userRouter  } from './modules/index.js'
-// import { NODE_ENV } from '../config/config.service.js'
+import { NODE_ENV } from '../config/config.service.js'
 import { GlobalError } from './common/utils/response/error.response.js';
 import cors from 'cors'
-// import { connectRedis , connectDB  } from './DB/index.js';
-// console.log({NODE_ENV});
+import { connectRedis , connectDB  } from './DB/index.js';
+console.log({NODE_ENV});
 async function bootstrap(){
 const app = express()
 
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(cors())
 
 // DB ....
-// await connectDB()
-// await connectRedis()
+await connectDB()
+await connectRedis()
 //application routing ......................
 app.get('/' , (req , res , next )=>{
     res.send('Hello')
