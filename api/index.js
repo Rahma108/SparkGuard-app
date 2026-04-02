6e7ee5148adc992024f4d0a4978c39d1854e601a
Rahma108 <salamarahma667@gmail.com>
2026-04-02 05:49:35 +0200

    crash vercel

--- api/index.js
+++ api/index.js
@@ -3,8 +3,13 @@ import bootstrap from "../src/app.bootstrap.js";
 let app;
 
 export default async function handler(req, res) {
-  if (!app) {
-    app = await bootstrap();
+  try {
+    if (!app) {
+      app = await bootstrap();
+    }
+    return app(req, res);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
   }
-  return app(req, res);
 }
