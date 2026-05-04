17dbe2cfa6608b5e8e064bc5e5fc2865cb4ea1d3
Rahma108 <salamarahma667@gmail.com>
2026-05-04 22:52:50 +0300

    Add Api Prediction With Integration AWS Model

--- src/modules/prediction/prediction.service.js
+++ src/modules/prediction/prediction.service.js
@@ -3,69 +3,69 @@ import { BadRequestException } from "../../common/utils/index.js";
 import axios from "axios";
 import { PredictionModel } from "../../DB/model/prediction.model.js";
 
-// export const checkReadingsService = async (readings) => {
-//     try {
-//         console.log("Calling FastAPI...");
-
-//         const response = await axios.post(
-//         "http://13.50.124.36:8000/predict",
-//         { readings },
-//         { timeout: 5000 }
-//         );
-
-//             const data = response.data;
-
-//                 save in DB
-//                 await PredictionModel.create({
-//                     result: data.result,
-//                     confidence: data.confidence,
-//                     variation: data.variation
-//                 });
-
-//                 return data;
-
-//     } catch (error) {
-//         throw BadRequestException({
-//         message: "Failed to connect to AI model ❌"
-//         });
-//     }
-// };
-
-
-// Test 
 export const checkReadingsService = async (readings) => {
     try {
-        console.log("INPUT:", readings);
+        console.log("Calling FastAPI...");
 
-        // mock logic (test only)
-        const result = "normal";
-        const confidence = 1;
-        const variation = 0;
+        const response = await axios.post(
+        "http://13.50.124.36:8000/predict",
+        { readings },
+        { timeout: 5000 }
+        );
 
-        // convert result → number
-        const prediction = result === "not normal" ? 1 : 0;
+            const data = response.data;
 
-        // save in DB
-        await PredictionModel.create({
-            result,
-            prediction,
-            confidence,
-            variation
-        });
+                save in DB
+                await PredictionModel.create({
+                    result: data.result,
+                    confidence: data.confidence,
+                    variation: data.variation
+                });
 
-        // return response
-        return {
-            result,
-            prediction,
-            confidence,
-            variation
-        };
+                return data;
 
     } catch (error) {
-        console.log(error);
         throw BadRequestException({
-            message: "Failed to connect to AI model ❌"
+        message: "Failed to connect to AI model ❌"
         });
     }
 };
+
+
+// Test 
+// export const checkReadingsService = async (readings) => {
+//     try {
+//         console.log("INPUT:", readings);
+
+//         // mock logic (test only)
+//         const result = "normal";
+//         const confidence = 1;
+//         const variation = 0;
+
+//         // convert result → number
+//         const prediction = result === "not normal" ? 1 : 0;
+
+//         // save in DB
+//         await PredictionModel.create({
+//             result,
+//             prediction,
+//             confidence,
+//             variation
+//         });
+
+//         // return response
+//         return {
+//             result,
+//             prediction,
+//             confidence,
+//             variation
+//         };
+
+//     } catch (error) {
+//         console.log(error);
+//         throw BadRequestException({
+//             message: "Failed to connect to AI model ❌"
+//         });
+//     }
+// };
 
